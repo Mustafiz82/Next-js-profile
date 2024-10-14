d98195519d01e7e694314469d116e6ec266c3761
unknown <sagormiah8204@gmail.com>
2024-10-14 16:11:16 +0600

    feat: Affilication , education and others add ui

--- app/Components/Education.jsx
+++ app/Components/Education.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { IoMdAdd } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Education = () => {
-    return (
-        <div>
-            <h1>Education tab</h1>
+  return (
+    <div>
+      <div>
+        <h3 className="text-[15px]">Education</h3>
+
+        <div className="border p-3 rounded-md">
+          <div className="text-[13px] py-2 px-3 bg-[#7367F029] rounded-md mb-14 flex justify-between">
+            <div>
+              <p>Dhaka University of Engineering and Technology</p>
+              <p>Computer Science and Engineering Bachelor of Engineering - BE</p>
+              <span>2016 - 2021</span>
+            </div>
+            <div className="flex gap-6 text-[19px] text-[#808390] pr-2">
+              <button>
+                <FaRegEdit />
+              </button>
+              <button>
+                <RiDeleteBin6Line />
+              </button>
+            </div>
+          </div>
+
+          <button className="flex items-center px-6 py-2 rounded-md border border-primary gap-2 justify-center text-[15px] text-primary">
+            <IoMdAdd className="text-lg" />
+            Add Education
+          </button>
         </div>
-    );
+
+        <div className="flex justify-end mt-10">
+          <button className="bg-primary text-white rounded-md py-2 px-8">
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
--- app/Components/Others.jsx
+++ app/Components/Others.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { IoMdAdd } from "react-icons/io";
 
 const Others = () => {
-    return (
-        <div>
-            <h1>Others</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="border rounded-md text-center py-4 mb-6">
+        <span className="text-[15px] font-semibold inline-block mb-1">Intrests</span>
+        <p className="text-sm mb-2">You currently don't have any interests added</p>
+
+        <button className="flex items-center px-6 py-2 rounded-md border border-primary gap-2 justify-center text-[15px] text-primary mx-auto ">
+          <IoMdAdd className="text-lg" />
+          Interests
+        </button>
+      </div>
+
+      <div className="border rounded-md text-center py-4">
+        <span className="text-[15px] font-semibold inline-block mb-1">Intrests</span>
+        <p className="text-sm mb-2">You currently don't have any interests added</p>
+
+        <button className="flex items-center px-6 py-2 rounded-md border border-primary gap-2 justify-center text-[15px] text-primary mx-auto ">
+          <IoMdAdd className="text-lg" />
+          Resume
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Others;
