01214c8c99b5e55784478d487b869896e9aab9e9
Mustafiz82 <mustafiz8260@gmail.com0>
2024-10-14 11:37:58 +0600

    folder structure created

---
+++ app/Components/Affiliation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Affiliation = () => {
+    return (
+        <div>
+            <h1>this is Affiliation</h1>
+        </div>
+    );
+};
+
+export default Affiliation;
